f4e692b088bf56085d9d7a5672c3006108b832b8
kjames2001 <62420081+kjames2001@users.noreply.github.com>
2023-04-16 08:06:00 +0200

    Update cleaner.py

--- cleaner.py
+++ cleaner.py
@@ -103,7 +103,7 @@ async def main():
         logging.info('Running media-tools script')
         await remove_stalled_sonarr_downloads()
         await remove_stalled_radarr_downloads()
-        logging.info('Finished running media-tools script. Sleeping for {API_TIMEOUT} seconds.')
+        logging.info('Finished running media-tools script. Sleeping for 10 minutes.')
         await asyncio.sleep(API_TIMEOUT)
 
 if __name__ == '__main__':
